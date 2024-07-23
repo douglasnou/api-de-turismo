418ed39793339543345e6f330e31fd3925c5e244
Douglas Nou <158287230+douglasnou@users.noreply.github.com>
2024-07-23 18:47:57 -0300

    servidor alterado

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import "dotenv/config";
 import { app } from "./src/app.js";
 const port = process.env.PORT || 3000
-app.listen(PORT, ()=>{
-    console.log(`This API was started at ${PORT}`);
+app.listen(port, ()=>{
+    console.log(`This API was started at ${port}`);
 });
 
 //
