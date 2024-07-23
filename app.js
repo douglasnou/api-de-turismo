d3e7ef9b2463d078e77e18b930fb2f5c018fe400
Douglas Nou <158287230+douglasnou@users.noreply.github.com>
2024-07-23 19:09:56 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { connectToDatabase } from "./config/dbConnection.js";
+import routes from "./routes/index.js";
+
+const connection = await connectToDatabase();
+
+connection.on("error", (error)=>{
+    console.error("erro de conexão", error);
+});
+
+connection.once("open", ()=>{
+    console.log("Sucesso de conexão.");
+});
+
+export const app = express();
+routes(app);
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
