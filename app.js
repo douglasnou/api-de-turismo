c579e764a8b4ce76eb1d3e83c4a0f766eefe3463
Douglas Nou <158287230+douglasnou@users.noreply.github.com>
2024-07-23 19:24:01 -0300

    aplicando alterações de acesso

--- app.js
+++ app.js
@@ -14,10 +14,3 @@ connection.once("open", ()=>{
 
 export const app = express();
 routes(app);
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
