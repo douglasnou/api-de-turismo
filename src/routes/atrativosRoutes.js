9f88f86d529f036972a435afe67b8d6103ddc347
Douglas Nou <158287230+douglasnou@users.noreply.github.com>
2024-07-23 20:38:45 -0300

    Add files via upload

--- src/routes/atrativosRoutes.js
+++ src/routes/atrativosRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { AtrativosControllers } from "../controllers/atrativosController.js";
+import { CorsMiddlewares } from "../middlewares/corsMiddlewares.js";
 
 const routes = express.Router();
 
-routes.get("/atrativos", AtrativosControllers.getAtrativos);
-routes.post("/atrativos", AtrativosControllers.postAtrativos);
-routes.put("/atrativos/:id", AtrativosControllers.updateAtrativo);
-routes.delete("/atrativos/:id", AtrativosControllers.deleteAtrativo);
+routes.get("/atrativos", CorsMiddlewares.corsPermission, AtrativosControllers.getAtrativos);
+routes.post("/atrativos", CorsMiddlewares.corsPermission, AtrativosControllers.postAtrativos);
+routes.put("/atrativos/:id", CorsMiddlewares.corsPermission, AtrativosControllers.updateAtrativo);
+routes.delete("/atrativos/:id", CorsMiddlewares.corsPermission, AtrativosControllers.deleteAtrativo);
 
 export default routes;
--- src/routes/index.js
+++ src/routes/index.js
@@ -8,16 +8,6 @@ const routes = (app) =>{
     ("Sucesso ao conectar."));
 
     app.use(express.json(), destinos, atrativos);
-
-    app.use((res, req, next)=>{
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-        res.header("Access-Control-Allow-Headers", "Content-Type");
-
-        app.use(cors());
-
-        next();
-    })
     
 };
 
