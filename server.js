5d8d35c22837970472c55a2a7a7660a2e6722d70
Douglas Nou <158287230+douglasnou@users.noreply.github.com>
2024-07-23 18:44:59 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 import "dotenv/config";
 import { app } from "./src/app.js";
-
-const PORT = 3000;
-
+const port = process.env.PORT || 3000
 app.listen(PORT, ()=>{
     console.log(`This API was started at ${PORT}`);
 });
